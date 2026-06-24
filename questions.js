e8b33316ef21f7cc952cff22f7e732e5fdc6ec6f
dwiatma <dwiatma@badak.or.id>
2026-06-24 15:09:15 +0700

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,311 @@
+const questions = [
+
+{
+no:1,
+text:"Aktif Berbicara",
+trait:"E",
+reverse:false
+},
+
+{
+no:2,
+text:"Mencari Kelemahan Orang Lain",
+trait:"A",
+reverse:true
+},
+
+{
+no:3,
+text:"Mengerjakan Tugas dengan Serius",
+trait:"C",
+reverse:false
+},
+
+{
+no:4,
+text:"Merasa Tertekan",
+trait:"N",
+reverse:false
+},
+
+{
+no:5,
+text:"Orang Unik yang Penuh dengan Gagasan Baru",
+trait:"O",
+reverse:false
+},
+
+{
+no:6,
+text:"Suka Menyendiri",
+trait:"E",
+reverse:true
+},
+
+{
+no:7,
+text:"Suka Menolong dan Tidak Iri pada Orang Lain",
+trait:"A",
+reverse:false
+},
+
+{
+no:8,
+text:"Bertindak Asal-asalan",
+trait:"C",
+reverse:true
+},
+
+{
+no:9,
+text:"Orang yang Santai dan Mampu Mengatasi Stres",
+trait:"N",
+reverse:true
+},
+
+{
+no:10,
+text:"Memiliki Rasa Ingin Tahu terhadap Sesuatu yang Berbeda",
+trait:"O",
+reverse:false
+},
+
+{
+no:11,
+text:"Penuh Tenaga",
+trait:"E",
+reverse:false
+},
+
+{
+no:12,
+text:"Berselisih dengan Orang Lain",
+trait:"A",
+reverse:true
+},
+
+{
+no:13,
+text:"Pekerja yang Dapat Diandalkan",
+trait:"C",
+reverse:false
+},
+
+{
+no:14,
+text:"Mudah Merasa Tegang",
+trait:"N",
+reverse:false
+},
+
+{
+no:15,
+text:"Orang yang Berbakat dan Pemikir",
+trait:"O",
+reverse:false
+},
+
+{
+no:16,
+text:"Dapat Membangkitkan Kegembiraan",
+trait:"E",
+reverse:false
+},
+
+{
+no:17,
+text:"Mudah Memaafkan",
+trait:"A",
+reverse:false
+},
+
+{
+no:18,
+text:"Cenderung Bekerja Tidak Teratur",
+trait:"C",
+reverse:true
+},
+
+{
+no:19,
+text:"Terlalu Sering Khawatir",
+trait:"N",
+reverse:false
+},
+
+{
+no:20,
+text:"Memiliki Imajinasi yang Aktif",
+trait:"O",
+reverse:false
+},
+
+{
+no:21,
+text:"Cenderung Pendiam",
+trait:"E",
+reverse:true
+},
+
+{
+no:22,
+text:"Secara Umum Dapat Dipercaya",
+trait:"A",
+reverse:false
+},
+
+{
+no:23,
+text:"Cenderung Pemalas",
+trait:"C",
+reverse:true
+},
+
+{
+no:24,
+text:"Memiliki Perasaan yang Stabil dan Tidak Mudah Sedih",
+trait:"N",
+reverse:true
+},
+
+{
+no:25,
+text:"Kreatif",
+trait:"O",
+reverse:false
+},
+
+{
+no:26,
+text:"Terbuka",
+trait:"E",
+reverse:false
+},
+
+{
+no:27,
+text:"Dingin dan Kurang Bersahabat",
+trait:"A",
+reverse:true
+},
+
+{
+no:28,
+text:"Dapat Memusatkan Diri Pada Pekerjaan",
+trait:"C",
+reverse:false
+},
+
+{
+no:29,
+text:"Dipengaruhi Oleh Suasana Hati",
+trait:"N",
+reverse:false
+},
+
+{
+no:30,
+text:"Menyukai Artistik dan Estetika",
+trait:"O",
+reverse:false
+},
+
+{
+no:31,
+text:"Orang yang Pemalu",
+trait:"E",
+reverse:true
+},
+
+{
+no:32,
+text:"Ramah dan Penuh Perhatian",
+trait:"A",
+reverse:false
+},
+
+{
+no:33,
+text:"Melakukan Sesuatu dengan Efisien",
+trait:"C",
+reverse:false
+},
+
+{
+no:34,
+text:"Merasa Tenang Meski dalam Situasi Menegangkan",
+trait:"N",
+reverse:true
+},
+
+{
+no:35,
+text:"Menyukai Pekerjaan yang Rutin",
+trait:"O",
+reverse:true
+},
+
+{
+no:36,
+text:"Suka Bergaul",
+trait:"E",
+reverse:false
+},
+
+{
+no:37,
+text:"Kadang Berperilaku Kasar",
+trait:"A",
+reverse:true
+},
+
+{
+no:38,
+text:"Merencanakan dan Memusatkan Pada Rencana Tersebut",
+trait:"C",
+reverse:false
+},
+
+{
+no:39,
+text:"Mudah Merasa Cemas",
+trait:"N",
+reverse:false
+},
+
+{
+no:40,
+text:"Merefleksi dan Mengolah Gagasan Baru",
+trait:"O",
+reverse:false
+},
+
+{
+no:41,
+text:"Kurang Tertarik dengan Seni",
+trait:"O",
+reverse:true
+},
+
+{
+no:42,
+text:"Menyukai Bekerjasama dengan Orang Lain",
+trait:"A",
+reverse:false
+},
+
+{
+no:43,
+text:"Memiliki Perhatian yang Mudah Terpecah",
+trait:"C",
+reverse:true
+},
+
+{
+no:44,
+text:"Memiliki Keahlian dalam Kesenian dan Literatur",
+trait:"O",
+reverse:false
+}
+
+];
